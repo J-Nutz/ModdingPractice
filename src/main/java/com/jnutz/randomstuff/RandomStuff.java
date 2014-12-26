b0dff317d765313e64d671bf3acb230115a50911
Jonah <rainman11@rocketmail.com>
2014-12-26 14:15:20 -0600

    added inits

---
+++ src/main/java/com/jnutz/randomstuff/RandomStuff.java
@@ -0,0 +1,35 @@
+package com.jnutz.randomstuff;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "randomstuff", name = "Random Stuff", version = "1.7.10-1.0" )
+public class RandomStuff {
+
+    @Mod.Instance("RandomStuff")
+    public static RandomStuff instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event){
+
+
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event){
+
+
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event){
+
+
+
+    }
+
+}
