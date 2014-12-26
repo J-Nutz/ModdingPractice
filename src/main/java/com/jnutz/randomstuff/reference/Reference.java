76900de057348d1db3a549ade5c925cb45acae15
Jonah <rainman11@rocketmail.com>
2014-12-26 15:29:53 -0600

    add reference class

---
+++ src/main/java/com/jnutz/randomstuff/reference/Reference.java
@@ -0,0 +1,15 @@
+package com.jnutz.randomstuff.reference;
+
+/**
+ * Created by Jonah on 12/26/2014.
+ */
+public class Reference {
+
+    public static final String MOD_ID = "RandomStuff";
+    public static final String MOD_NAME = "Random Stuff";
+    public static final String VERSION = "1.7.10-1.0";
+    public static final String CLIENT_PROXY_CLASS = "com.jnutz.randomstuff.proxy.ClientProxy";
+    public static final String SERVER_PROXY_CLASS = "com.jnutz.randomstuff.proxy.ServerProxy";
+
+
+}
