4bd8b5749d433c82e3de0936e07a010a324558c8
Jonah <rainman11@rocketmail.com>
2014-12-26 22:17:44 -0600

    config gui not working

---
+++ src/main/java/com/jnutz/randomstuff/client/gui/ModGuiConfig.java
@@ -0,0 +1,23 @@
+package com.jnutz.randomstuff.client.gui;
+
+import com.jnutz.randomstuff.handler.ConfigurationHandler;
+import com.jnutz.randomstuff.reference.Reference;
+import cpw.mods.fml.client.config.GuiConfig;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraftforge.common.config.ConfigElement;
+import net.minecraftforge.common.config.Configuration;
+
+public class ModGuiConfig extends GuiConfig {
+
+    public ModGuiConfig(GuiScreen guiScreen){
+
+        super(guiScreen,
+        new ConfigElement(ConfigurationHandler.configuration.getCategory(Configuration.CATEGORY_GENERAL)).getChildElements(),
+                Reference.MOD_ID,
+                false,
+                false,
+                GuiConfig.getAbridgedConfigPath(ConfigurationHandler.configuration.toString()));
+
+    }
+
+}
