d7cf835030ebf4b37bdcd29fa1f824d30c74eb8b
Jnutz <rainman11@rocketmail.com>
2014-12-29 18:17:47 -0600

    fixed mcmod.info gui

--- src/main/java/com/jnutz/randomstuff/reference/Reference.java
+++ src/main/java/com/jnutz/randomstuff/reference/Reference.java
@@ -10,6 +10,7 @@ public class Reference {
     public static final String VERSION = "1.7.10-1.0";
     public static final String CLIENT_PROXY_CLASS = "com.jnutz.randomstuff.proxy.ClientProxy";
     public static final String SERVER_PROXY_CLASS = "com.jnutz.randomstuff.proxy.ServerProxy";
+    public static final String GUI_FACTORY_CLASS = "com.jnutz.randomstuff.client.gui.GuiFactory";
 
 
 }
