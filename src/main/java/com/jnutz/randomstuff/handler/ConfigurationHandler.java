471bd2467cabca31c06710bdbbe789ac5fa5a9dc
Jnutz <rainman11@rocketmail.com>
2014-12-29 18:16:42 -0600

    fixed mcmod.info

--- src/main/java/com/jnutz/randomstuff/handler/ConfigurationHandler.java
+++ src/main/java/com/jnutz/randomstuff/handler/ConfigurationHandler.java
@@ -4,50 +4,40 @@ import com.jnutz.randomstuff.reference.Reference;
 import cpw.mods.fml.client.event.ConfigChangedEvent;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.common.config.Configuration;
+
 import java.io.File;
 
-public class ConfigurationHandler {
-
+public class ConfigurationHandler
+{
     public static Configuration configuration;
     public static boolean testValue = false;
 
-    public static void init(File configFile){
-
-        //Creates The Configuration Based On Mod Name
-        if(configuration == null) {
-
-            Configuration configuration = new Configuration(configFile);
+    public static void init(File configFile)
+    {
+        // Create the configuration object from the given configuration file
+        if (configuration == null)
+        {
+            configuration = new Configuration(configFile);
             loadConfiguration();
-
         }
-        //Move somewhere
-        //Gets the config, in general category, adds in config value with default true, Adds in comment for what the config is for, then gets the value
-        //Can change "Configuration.CATEGORY_GENERAL to and String such as "Blocks", "Items", "Power", etc.
-
     }
 
+    private static void loadConfiguration()
+    {
+        testValue = configuration.getBoolean("configValue", Configuration.CATEGORY_GENERAL, false, "This is an example configuration value");
+
+        if (configuration.hasChanged())
+        {
+            configuration.save();
+        }
+    }
 
     @SubscribeEvent
-    public void onConfigurationChangeEvent(ConfigChangedEvent.OnConfigChangedEvent event){
-
-        if(event.modID.equalsIgnoreCase(Reference.MOD_ID)){
-
+    public void onConfigurationChangedEvent(ConfigChangedEvent.OnConfigChangedEvent event)
+    {
+        if (event.modID.equalsIgnoreCase(Reference.MOD_ID))
+        {
             loadConfiguration();
-
         }
-
     }
-
-    private static void loadConfiguration(){
-
-        testValue = configuration.getBoolean("configValue", configuration.CATEGORY_GENERAL, false, "Example Config Line");
-
-        if(configuration.hasChanged()){
-
-            configuration.save();
-
-        }
-
-    }
-
 }
