c666fb8408f3aec8f748cd63f5e2c182a50c6288
Jnutz <rainman11@rocketmail.com>
2014-12-29 22:53:33 -0600

    added new texture for maple leaf

--- src/main/java/com/jnutz/randomstuff/item/itemMapleLeaf.java
+++ src/main/java/com/jnutz/randomstuff/item/itemMapleLeaf.java
@@ -1,11 +1,15 @@
 package com.jnutz.randomstuff.item;
 
+import net.minecraft.creativetab.CreativeTabs;
+
 public class itemMapleLeaf extends itemRS{
 
+    //Setting Name From itemRS Pre-Defined Class
     public itemMapleLeaf(){
 
         super();
         this.setUnlocalizedName("mapleLeaf");
+        this.setCreativeTab(CreativeTabs.tabMisc);
 
     }
 
